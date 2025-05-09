72ece5ee656c620606d42e87d7e5e8ba94862701
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-05-09 17:41:24 +0300

    feat: add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { UiComponent } from './ui/ui.component';
 
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [UiComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
