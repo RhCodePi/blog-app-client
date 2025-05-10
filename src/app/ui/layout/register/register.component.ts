65d7572fcbc0883a60b5c4bd9e0b0294d86cdf43
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-05-10 19:03:10 +0300

    feat(register): implement logic and update UI

--- src/app/ui/layout/register/register.component.ts
+++ src/app/ui/layout/register/register.component.ts
@@ -1,12 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../services/common/models/user.service';
+import {
+  AbstractControl,
+  ReactiveFormsModule,
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { User } from '../../../entities/user';
+import { CreateUser } from '../../../contracts/users/create-user';
+import { CommonModule, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgClass, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  constructor(
+    private userService: UserService,
+    private formBuilder: UntypedFormBuilder
+  ) {}
 
+  frm: UntypedFormGroup;
+  ngOnInit(): void {
+    this.frm = this.formBuilder.group(
+      {
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(50),
+          ],
+        ],
+        email: [
+          '',
+          [Validators.required, Validators.email, Validators.maxLength(250)],
+        ],
+        password: ['', Validators.required],
+        passwordConfirm: ['', Validators.required],
+      },
+      {
+        validators: (group: AbstractControl): ValidationErrors | null => {
+          let password = group.get('password').value;
+          let passwordConfirm = group.get('passwordConfirm').value;
+
+          return password === passwordConfirm ? null : { notsame: true };
+        },
+      }
+    );
+  }
+
+  get component() {
+    return this.frm.controls;
+  }
+
+  submitted: boolean = false;
+  async onSubmit(user: User) {
+    this.submitted = true;
+
+    this.component['email'].errors;
+
+    this.frm;
+
+    debugger;
+    if (this.frm.invalid) return;
+
+    const result: CreateUser = await this.userService.create(user);
+
+    if (result.succeeded) {
+      console.log('User Created!');
+      console.log(result.message);
+    } else {
+      console.log('Error!');
+      console.log(result.message);
+    }
+  }
 }
