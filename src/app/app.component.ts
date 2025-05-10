0f535010af87e03ef4d7790d8e722e221855baa6
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-05-10 14:56:44 +0300

    feat: implemnt register page and global navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 import { RouterOutlet } from '@angular/router';
+import { UiComponent } from "./ui/ui.component";
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './ui/layout/login/login.component';
 import { RegisterComponent } from './ui/layout/register/register.component';
-import { UiComponent } from './ui/ui.component';
 
 export const routes: Routes = [
-    {path:'', component:UiComponent},
     {path:'register', component:RegisterComponent},
     {path:'login', component:LoginComponent}
 ];
