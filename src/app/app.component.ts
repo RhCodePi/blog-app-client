afafecde6d2640e5d996c324ec3ab68393759226
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-05-10 18:47:11 +0300

    chore(appconfig): enable HTTP support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { RouterOutlet } from '@angular/router';
 import { UiComponent } from "./ui/ui.component";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UiComponent],
+  imports: [RouterOutlet, UiComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
