08f048c6e274ec553807ef115f4b687f675e7086
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-05-16 20:56:21 +0300

    refactor: update logic and ui

--- src/app/ui/layout/layout.component.ts
+++ src/app/ui/layout/layout.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { AuthService } from '../../services/common/auth.service';
+import {
+  CustomToastrService,
+  ToastrMessageType,
+} from '../../services/common/custom-toastr.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  styleUrl: './layout.component.scss',
 })
 export class LayoutComponent {
-
+  constructor(
+    public authService: AuthService,
+    private toastr: CustomToastrService,
+    private router: Router
+  ) {}
+  signOut() {
+    localStorage.removeItem('access_token');
+    this.authService.identityCheck();
+    this.router.navigate(['']);
+    this.toastr.alert(
+      'sign out success',
+      'Sign Out',
+      ToastrMessageType.SUCCESS,
+      {
+        progressBar: true,
+      }
+    );
+  }
 }
--- src/app/ui/layout/login/login.component.ts
+++ src/app/ui/layout/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../../services/common/models/user.service';
-import { CustomToastrService, ToastrMessagePositon, ToastrMessageType } from '../../../services/common/custom-toastr.service';
+import {
+  CustomToastrService,
+  ToastrMessagePositon,
+  ToastrMessageType,
+} from '../../../services/common/custom-toastr.service';
+import { AuthService } from '../../../services/common/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,27 +15,41 @@ import { CustomToastrService, ToastrMessagePositon, ToastrMessageType } from '..
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  constructor(private userService: UserService, private toastr: CustomToastrService) {}
+  constructor(
+    private userService: UserService,
+    private toastr: CustomToastrService,
+    private authService: AuthService
+  ) {}
 
-  async login(){
-    const usernameOrEmail = document.getElementById("emailOrUsername") as HTMLInputElement;
-    const password = document.getElementById("password") as HTMLInputElement;
+  async login() {
+    const usernameOrEmail = document.getElementById(
+      'emailOrUsername'
+    ) as HTMLInputElement;
+    const password = document.getElementById('password') as HTMLInputElement;
 
-    const result = await this.userService.login(usernameOrEmail.value, password.value);
+    const result = await this.userService.login(
+      usernameOrEmail.value,
+      password.value
+    );
 
-    if(result === undefined) return;
+    if (result === undefined) return;
 
-    if(result.isSuccess)
-    {
-      this.toastr.alert(result.message, "Success", ToastrMessageType.SUCCESS, {
-        progressBar: true
-      })
-    }
-    else {
-      this.toastr.alert(result.message, "Error", ToastrMessageType.ERROR, {
+    if (result.isSuccess) {
+      if (result.token) {
+        localStorage.setItem('access_token', result.token.accessToken);
+        localStorage.setItem('refresh_token', result.token.refreshToken);
+      }
+
+      this.authService.identityCheck();
+
+      this.toastr.alert(result.message, 'Success', ToastrMessageType.SUCCESS, {
+        progressBar: true,
+      });
+    } else {
+      this.toastr.alert(result.message, 'Error', ToastrMessageType.ERROR, {
         position: ToastrMessagePositon.BOTTOM_RIGHT,
-        progressBar: true
-      })
+        progressBar: true,
+      });
     }
   }
 }
