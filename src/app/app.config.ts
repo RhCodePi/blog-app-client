0804e8376d16856900f39c65220d0747ab0ba364
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-05-16 20:51:41 +0300

    chore(appconfig): add jwt support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,6 +6,7 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { JwtModule } from '@auth0/angular-jwt';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,6 +18,19 @@ export const appConfig: ApplicationConfig = {
       useValue: 'https://localhost:7166/api',
     },
     provideAnimations(),
-    provideToastr()
+    provideToastr(),
+    importProvidersFrom(
+      JwtModule.forRoot({
+        config: {
+          tokenGetter: tokenGetter,
+          allowedDomains: ["https://localhost:7166", "http://localhost:5008"],
+        }
+      })
+    )
+
   ],
 };
+
+export function tokenGetter(){
+  return localStorage.getItem("access_token");
+}
