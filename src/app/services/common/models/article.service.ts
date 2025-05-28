17609f1cff244a3ab29d1da7a513136167522a2f
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-05-28 20:57:31 +0300

    feat: add article service

---
+++ src/app/services/common/models/article.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClientService } from '../http-client.service';
+import { firstValueFrom } from 'rxjs';
+import { Article } from '../../../contracts/articles/article';
+import { HttpHeaders } from '@angular/common/http';
+import { StorageService } from '../storage.service';
+
+
+const REFRESH_TOKEN: string = 'refresh_token'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ArticleService {
+  constructor(
+    private httpClient: HttpClientService,
+    private storageService: StorageService
+  ) {}
+  async getUserArticles(): Promise<Article[]> {
+    const headers = this.addAuthToHeaders();
+
+    const observable = this.httpClient.post<Article[] | any>(
+      {
+        controller: 'articles',
+        action: 'getuserarticles',
+        headers: headers,
+      },
+      {
+        refreshToken: this.storageService.getLocalStorage(REFRESH_TOKEN),
+      }
+    );
+
+    debugger;
+
+    return (await firstValueFrom(observable)) as Article[];
+  }
+
+  private addAuthToHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.storageService.getLocalStorage(
+        'access_token'
+      )}`,
+    });
+  }
+}
