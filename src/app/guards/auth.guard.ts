609c4b16d899b56f240127241a956a507b94cf81
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-05-28 20:57:16 +0300

    feat: add auth guard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,18 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthHelperService } from '../services/helpers/auth-helper.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authHelper = inject(AuthHelperService);
+  const router = inject(Router);
+
+  authHelper.identityCheck();
+
+  if (!authHelper.isAuthenticated) {
+    router.navigate(['login']);
+
+    return false;
+  }
+
+  return true;
+};
