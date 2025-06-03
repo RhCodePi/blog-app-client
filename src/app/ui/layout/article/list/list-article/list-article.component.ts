f1eae2436d341d13aa05ca981e4e0c025552a7fd
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-06-03 21:29:38 +0300

    feat: implement the list article

--- src/app/ui/layout/article/list/list-article/list-article.component.ts
+++ src/app/ui/layout/article/list/list-article/list-article.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Article } from '../../../../../contracts/articles/article';
+import { ArticleService } from '../../../../../services/common/models/article.service';
 
 @Component({
   selector: 'app-list-article',
   standalone: true,
-  imports: [],
+  imports: [MatPaginatorModule, MatTableModule, MatButtonModule, MatIconModule],
   templateUrl: './list-article.component.html',
-  styleUrl: './list-article.component.scss'
+  styleUrl: './list-article.component.scss',
 })
-export class ListArticleComponent {
+export class ListArticleComponent implements OnInit {
+  displayedColumns: string[] = ['title', 'createdDate', 'updatedDate', 'actions'];
+  dataSource = new MatTableDataSource<Article>();
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private articleService: ArticleService){}
+
+
+  async ngOnInit(): Promise<void> {
+
+    var result = await this.articleService.getUserArticles()
+
+    if(result === undefined) return;
+
+    this.dataSource.data = result
+
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  onEdit(article: Article) {
+    console.log('Edit:', article);
+  }
+
+  onDelete(article: Article) {
+  }
 
 }
