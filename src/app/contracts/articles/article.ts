8364f347a54c41b9678747e942a739d583adca88
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-06-17 00:20:31 +0300

    feat: implement OnEdit method

--- src/app/contracts/articles/article.ts
+++ src/app/contracts/articles/article.ts
@@ -1,6 +1,7 @@
 export class Article {
     id: string;
     title: string;
+    content: string;
     createdDate: Date;
     updatedDate?: Date;
 }
--- src/app/ui/layout/article/list/list-article/list-article.component.ts
+++ src/app/ui/layout/article/list/list-article/list-article.component.ts
@@ -5,6 +5,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Article } from '../../../../../contracts/articles/article';
 import { ArticleService } from '../../../../../services/common/models/article.service';
+import { MatDialog } from '@angular/material/dialog';
+import { EditArticleDialogComponent } from '../../../../../dialogs/edit-article-dialog/edit-article-dialog.component';
 
 @Component({
   selector: 'app-list-article',
@@ -14,33 +16,53 @@ import { ArticleService } from '../../../../../services/common/models/article.se
   styleUrl: './list-article.component.scss',
 })
 export class ListArticleComponent implements OnInit {
-  displayedColumns: string[] = ['title', 'createdDate', 'updatedDate', 'actions'];
+  displayedColumns: string[] = [
+    'title',
+    'createdDate',
+    'updatedDate',
+    'actions',
+  ];
   dataSource = new MatTableDataSource<Article>();
-  
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private articleService: ArticleService){}
-
+  constructor(
+    private articleService: ArticleService,
+    private dialog: MatDialog
+  ) {}
 
   async ngOnInit(): Promise<void> {
-
-    var result = await this.articleService.getUserArticles()
-
-    if(result === undefined) return;
-
-    this.dataSource.data = result
-
+    await this.getUser();
   }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
 
-  onEdit(article: Article) {
-    console.log('Edit:', article);
+   onEdit(event: MouseEvent,article: Article) {
+    (event.currentTarget as HTMLElement).blur();
+    console.log('Edit:', article); //todo craete modal
+    this.dialog.open(EditArticleDialogComponent, {
+      width: '450px',
+      data: article,
+    }).afterClosed().subscribe(async (result) => {
+      if(result){
+        await this.getUser()
+      }
+    });
   }
 
   onDelete(article: Article) {
+  }
+
+  private async getUser(){
+    var result = await this.articleService.getUserArticles();
+
+    result.map(p => p.content = JSON.parse(p.content))
+
+    if (result === undefined) return;
+
+    this.dataSource.data = result;
   }
 
 }
